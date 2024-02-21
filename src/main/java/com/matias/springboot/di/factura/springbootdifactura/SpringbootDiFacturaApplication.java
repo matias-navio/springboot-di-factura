8aba62779e26060e687fee8a9907a6e0e9a25654
Matias Navio <naviomatias2@gmail.com>
2024-02-21 09:34:16 -0300

    Initial commit

---
+++ src/main/java/com/matias/springboot/di/factura/springbootdifactura/SpringbootDiFacturaApplication.java
@@ -0,0 +1,13 @@
+package com.matias.springboot.di.factura.springbootdifactura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDiFacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDiFacturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matias/springboot/di/factura/springbootdifactura/SpringbootDiFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.springboot.di.factura.springbootdifactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
